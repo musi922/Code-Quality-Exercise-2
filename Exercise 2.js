11b1e5963e1743e806af096c7840461406cf7577
Richard <richardmusime6@gmail.com>
2024-08-22 20:22:52 +0200

    Removed the sqrt method and used pure number

--- Exercise 2.js
+++ Exercise 2.js
@@ -1,6 +1,6 @@
 const isPrime = number =>{
     if (number < 2) return false;
-    for (let i = 2; i <= Math.sqrt(number); i++) {
+    for (let i = 2; i <number; i++) {
         if (number % i === 0) return false ;
     }
     return true;
