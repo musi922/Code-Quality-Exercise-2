1f49fca29e1d7ae04b40c8d0f86b3c510ed6d8f2
Richard <richardmusime6@gmail.com>
2024-08-22 18:59:58 +0200

    Removed the testing console

--- Exercise 2.js
+++ Exercise 2.js
@@ -1,9 +1,7 @@
 const isPrime = number =>{
     if (number < 2) return false;
     for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) return false 
+        if (number % i === 0) return false ;
     }
-    return true
+    return true;
 }
-
-console.log(isPrime(4));
